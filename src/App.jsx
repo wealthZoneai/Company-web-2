6593459c58f4a8b82f5418339ecb781dd14c101a
Ravi Teja <batturaviteja18@gmail.com>
2025-12-30 17:06:41 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import React, { useState } from "react";
 import AppRouter from "./Router/appRouter";
 import Navbar from "./pages/home/Navbar";
 import { Route, Routes } from "react-router-dom";
+import Hero from "./pages/home/Hero";
 import Home from "./pages/home/home";
+
+
 
 function App() {
 
   return (
-    <div>
-      <Navbar />
+    <>
+      <Navbar/>
       <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<Abou/>} /> */}
+        <Route path="/" element={<Home/>}/>
       </Routes>
-    </div>
+      
+    </>
   );
 }
 
--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,25 +1,7 @@
 import React from 'react'
-import Navbar from './Navbar'
-import Programs from './Programs'
-import StatsBar from './StatsBar'
-import WhyWZG from './WhyWZG'
-import Services from './Services'
-import Testimonals from './Testimonals'
-import Partners from './Partners'
-import CallToAction from './CallToAction'
 
 export default function Hero() {
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <Programs />
-      <StatsBar />
-      <WhyWZG />
-      <Services />
-      <Testimonals />
-      <Partners />
-      <CallToAction />
-    </>
+    <div>Hero</div>
   )
 }
